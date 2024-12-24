55fdf6712cb1c1492c4ffdd0b72af2e51c7af2af
HANMSEN <marcelinjeanfrantzolsen@gmail.com>
2024-12-24 14:33:05 -0500

    Create Antispam shield.js

---
+++ Antispam shield.js
@@ -0,0 +1,26 @@
+const { bot, sleep } = require('../lib/')
+
+bot(
+	{
+		pattern: 'antishield ?(.*)',
+		fromMe: true,
+		desc: 'antispam shield',
+		type: 'Antispam shield',
+	},
+	async (message, match) => {
+		const a = '💠 _Antispam shield_ => [ ```Activated``` ]'
+		const b = '💠 Antispam shield\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n💠 Antispam shield\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n💠 Antispam shield\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n💠 Antispam shield\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n💠 Antispam shield\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n💠 Antispam shield\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n💞 ```Antispam shield \n  by Prabhath Bandara``` 💫'
+  		const c = '💠 Antispam shield 💫\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n💠 Antispam shield\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n💠 Antispam shield\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n💠 Antispam shield\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n💞 ```Antispam shield \n  by Prabhath Bandara``` 💫'
+		
+		const msg = await message.send(a)
+       		await sleep(500)
+		await message.send({ key: msg.key, text: b }, {}, 'edit')
+
+		let j = 0
+		while (j < 5) {
+			await sleep(300)
+            		await message.send(c)
+			j++
+		}
+	}
+)
